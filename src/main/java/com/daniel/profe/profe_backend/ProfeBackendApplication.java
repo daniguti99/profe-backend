f99773633705e7a46c2b7ebbbf9357ee9f26f5cc
daniguti99 <dgm1021999@gmail.com>
2026-09-23 12:51:57 +0200

    first commit

---
+++ src/main/java/com/daniel/profe/profe_backend/ProfeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.profe.profe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/profe/profe_backend/ProfeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.profe.profe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
